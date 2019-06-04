604476070140e248b0e04b1037d4b8fdfae2aa9d
jeffreyxdc <jeffreyxdc@gmail.com>
2019-06-04 14:50:02 -0400

    added ability to get current location in lat and long

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      base: [],
+    }
+  }
+
+  handleInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  getCurrentLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.setState({
+          base: [position.coords.latitude, position.coords.longitude]
+        });
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }
+
   render() {
     return (
       <div className="App">
+        <h1>Average Joe</h1>
 
+        <form>
+          <input
+            type="text" 
+            name="base" 
+            placeholder="Starting Location"
+            value={this.state.base}
+            onChange={this.handleInput} />
+          <button type="button" onClick={this.getCurrentLocation}>Current Location</button>
+        </form>
+        
       </div>
     );
   }
