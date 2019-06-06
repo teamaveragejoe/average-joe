5a1501614043e62bd39cd5a43f2dc33f733ef598
fazaa <fazaa.ahmed93@gmail.com>
2019-06-06 14:17:49 -0400

    display and refactored directions list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Axios from 'axios';
 import Form from './Form.js';
 import Map from './Map.js';
+import Directions from './Directions.js';
 
 class App extends Component {
   constructor() {
@@ -212,33 +213,49 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header>
-          <h1>Average Joe</h1>
-        </header>
+      <div className="flex-container">
+        <div className="App">
+          <header>
+            <h1>Average Joe</h1>
+          </header>
 
-        <Form 
-          search={this.search}
-          base={this.state.base}
-          handleInput={this.handleInput}
-          getCurrentLocation={this.getCurrentLocation}
-          searchTerm={this.state.searchTerm}
-        />
+          <Form 
+            search={this.search}
+            base={this.state.base}
+            handleInput={this.handleInput}
+            getCurrentLocation={this.getCurrentLocation}
+            searchTerm={this.state.searchTerm}
+          />
 
-        <div className="content-container">
-          <div className="location-list">
-            {this.state.searchResults.map(location => {
-              return (
-                <button key={location.address} onClick={() => { this.setDestination(location.address)}}>
-                  <h4>{location.name}</h4>
-                  <p>{location.address}</p>
-                </button>
-              )
-            })}
+          <div className="content-container">
+            <div className="location-list">
+              {this.state.searchResults.map(location => {
+                return (
+                  <button key={location.address} onClick={() => { this.setDestination(location.address)}}>
+                    <h4>{location.name}</h4>
+                    <p>{location.address}</p>
+                  </button>
+                )
+              })}
+            </div>
+
+            <Map url={this.state.mapImageURL}/>
+
+            
+            <div className="directions-list">
+              <ol>
+              {this.state.directions.map(directions => {
+                return (
+                  <Directions 
+                    item={directions}
+                  />   
+                )
+              })}
+              </ol>
+            </div>
           </div>
-
-          <Map url={this.state.mapImageURL}/>
         </div>
+        
       </div>
     );
   }
